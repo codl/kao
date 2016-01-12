e1dc4b79c43aacadfce1fca6df4c488cb79b235b
codl (Corentin Delcourt) <codl@codl.fr>
2016-01-12 17:50:25 +0100

    fix for firefox not supporting let yet, and also not having innerText

--- kao.js
+++ kao.js
@@ -1,20 +1,23 @@
 "use strict";
 var inputs = Array.from(document.querySelectorAll(".inputs a"));
 var textarea = document.querySelector("textarea");
-for(let el of inputs){
+function makeMoji(el){
     el.addEventListener("click", function(){
         if("selectionStart" in textarea){
             let start = textarea.selectionStart;
             let end = textarea.selectionEnd;
             let left = textarea.value.slice(0, start);
             let right = textarea.value.slice(end);
-            textarea.value = left + el.innerText + right;
+            textarea.value = left + el.textContent + right;
             textarea.selectionStart = textarea.selectionEnd = start + 1;
         }
         else {
-            textarea.value += el.innerText;
+            textarea.value += el.textContent;
         }
     });
+}
+for(var el of inputs){
+    makeMoji(el);
 }
 
 var copy = document.querySelector("button");
