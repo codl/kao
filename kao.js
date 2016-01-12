002b5aa843c60fbb047eec60bb7ffc041b7e7699
codl (Corentin Delcourt) <codl@codl.fr>
2016-01-12 14:16:02 +0100

    support cursor position & selection replacement

--- kao.js
+++ kao.js
@@ -3,7 +3,17 @@ var inputs = Array.from(document.querySelectorAll(".inputs a"));
 var textarea = document.querySelector("textarea");
 for(let el of inputs){
     el.addEventListener("click", function(){
-        textarea.value += el.innerText;
+        if("selectionStart" in textarea){
+            let start = textarea.selectionStart;
+            let end = textarea.selectionEnd;
+            let left = textarea.value.slice(0, start);
+            let right = textarea.value.slice(end);
+            textarea.value = left + el.innerText + right;
+            textarea.selectionStart = textarea.selectionEnd = start + 1;
+        }
+        else {
+            textarea.value += el.innerText;
+        }
     });
 }
 
